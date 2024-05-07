6925ec4227f0cf500999c2f7866b9db1a84d2abd
Vladyslava Kanivets <kanivetsvlada21@gmail.com>
2024-05-07 21:04:17 +0200

    implemented hibernate mapping and PlayerRepositoryDB methods

--- src/main/java/com/game/entity/Player.java
+++ src/main/java/com/game/entity/Player.java
@@ -1,23 +1,33 @@
 package com.game.entity;
 
+import jakarta.persistence.*;
+
 import java.util.Date;
 
-
+@Entity
+@Table(name="player", schema = "rpg")
+@NamedQuery(name = "player_count",
+        query = "SELECT COUNT(p) FROM Player p")
 public class Player {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
+    @Column(length = 12, nullable = false)
     private String name;
-
+    @Column(length = 30, nullable = false)
     private String title;
-
+    @Column(nullable = false)
+    @Enumerated(EnumType.ORDINAL)
     private Race race;
-
+    @Column(nullable = false)
+    @Enumerated(EnumType.ORDINAL)
     private Profession profession;
-
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
     private Date birthday;
-
+    @Column(nullable = false)
     private Boolean banned;
-
+    @Column(nullable = false)
     private Integer level;
 
     public Player() {
